24f6cf07f41b53d04015b546983ac11a915d7da6
Diane Corriette <dcorriette@gmail.com>
2025-04-18 19:53:54 +0100

    Add blog templates to main project area. Include social share and blog breadcrumbs

--- ai_marketing/settings.py
+++ ai_marketing/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'ai_marketing.middleware.AuthRequiredMiddleware', 
+    # 'ai_marketing.middleware.AuthRequiredMiddleware', 
 ]
 
 # Authentication settings
