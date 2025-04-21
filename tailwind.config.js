27f7f6e106f448747deb610fe503e92e6dc8bddf
djangify <djangify@gmail.com>
2025-04-21 19:52:12 +0100

    Update buttons with btn-main in css and update other templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
         main: "#9810fa",
         secondary: "#8C8391",
         accent: "#E6C5EB",
-        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+
       },
       fontFamily: {
         sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
